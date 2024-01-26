32f6ab108fc70bc502c7c8685ac47227532bdbf1
Carlos Cabanero <carlos@blink.sh>
2024-01-26 18:54:18 -0500

    Initial Commit

---
+++ TeamsUI/TeamsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamsUIApp.swift
+//  TeamsUI
+//
+//  Created by Carlos Cabanero on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TeamsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
